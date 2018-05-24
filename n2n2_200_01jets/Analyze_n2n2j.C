ff790dad52166a8bb1de7314f122e0ccff835922
Zachary Flowers <zacharyflowers96@gmail.com>
2018-05-24 16:53:09 -0500

    Add MET Smearing Function

--- n2n2_200_01jets/Analyze_n2n2j.C
+++ n2n2_200_01jets/Analyze_n2n2j.C
@@ -6,17 +6,24 @@
     #include <stdio.h>
     #include <string>
 
+// smeared MET given true MET and sumPT
+TVector3 Smear_MET(const TVector3& PT_true, double sum_PT);
+
+//take two tgrapherrors, plot, them and fit them, save output to a png
+void Plot_Res_vs_SumPT(TGraphErrors* gr_perp, TGraphErrors* gr_par);
+
 Double_t Fit_sqrt(Double_t *x,Double_t *par)
 {
-    double con1=par[0];
-    double con2=par[1];
+    double con0=par[0];
+    double con1=par[1];
+    double con2=par[2];
     double fitnum=0.0;
     /*if(fitnum < 1e-12 || fitnum == nan)
      {
      cout << "FIT FAIL" << endl;
      return 0.;
      }*/
-    fitnum=sqrt((con1*con1)/x[0]+con2*con2);
+    fitnum=sqrt((con0*con0)/(x[0]*x[0])+(con1*con1)/x[0]+con2*con2);
     return fitnum;
 }
 
@@ -45,7 +52,10 @@ TGraphErrors* TGE_TF1(TGraphErrors *plot, TF1 *fit_func)
 
     void Analyze_n2n2j::Loop()
     {
+        gRandom = new TRandom3();
         //gStyle->SetOptStat(000002000);
+        //gStyle->SetOptStat(1);
+        gStyle->SetOptFit(1);
         double MET_Parallel_Mag=0.0;
         double MET_Perpendicular_Mag=0.0;
         TVector3 MET_Perpendicular;
@@ -60,6 +70,7 @@ TGraphErrors* TGE_TF1(TGraphErrors *plot, TF1 *fit_func)
         TCanvas *c3 = new TCanvas("c3","Canvas for MET Parallel",750,500);
         TCanvas *c4 = new TCanvas("c4","Canvas for MET Perpendicular",750,500);
         TCanvas *c5 = new TCanvas("c5","Canvas for Sum PT (MC)",750,500);
+        TCanvas *c7 = new TCanvas("c7","Canvas for MET RECO",750,500);
         
         TH2F *hist_pythia_Pt_vs_delphes_Pt_n2n2j = new TH2F("hist_pythia_Pt_vs_delphes_Pt_n2n2j","Pythia Jets Pt vs Delphes Jets Pt", 100, 0.0, 1500.0, 100, 0.0, 1500.0);
         
@@ -69,6 +80,8 @@ TGraphErrors* TGE_TF1(TGraphErrors *plot, TF1 *fit_func)
         TH1D *hist_MET_Parallel = new TH1D("hist_MET_Parallel","MET_Parallel",100,-100.0,100.0);
         TH1D *hist_MET_Perpendicular = new TH1D("hist_MET_Perpendicular","MET_Perpendicular",100,-100.0,100.0);
         TH1D *hist_PT_true = new TH1D("hist_PT_true","Histogram of Sum PT MC",100,0.0,1200.0);
+        
+        TH1D *hist_MET_RECO = new TH1D("hist_MET_RECO","Smeared PT True (MET)",100,0.0,800.0);
         
         std::vector<TH1D*> vect_hist_MET_Parallel;
         std::vector<TH1D*> vect_hist_MET_Perpendicular;
@@ -102,12 +115,12 @@ TGraphErrors* TGE_TF1(TGraphErrors *plot, TF1 *fit_func)
            TVector3 n1A_vect=n1A_MC->Vect();
            TVector3 n1B_vect=n1B_MC->Vect();
            PT_true=n1A_vect+n1B_vect;
-           TVector3 MET_RECO=*MET;
+           TVector3 MET_Delphes=*MET;
            PT_true.SetZ(0.0);
-           MET_RECO.SetZ(0.0);
+           MET_Delphes.SetZ(0.0);
 
-           MET_Perpendicular_Mag=PT_true.Cross(BeamSpot).Unit().Dot(MET_RECO);
-           MET_Parallel_Mag=(MET_RECO.Dot(PT_true))/PT_true.Mag();
+           MET_Perpendicular_Mag=PT_true.Cross(BeamSpot).Unit().Dot(MET_Delphes);
+           MET_Parallel_Mag=(MET_Delphes.Dot(PT_true))/PT_true.Mag();
            
            hist_MET_Parallel->Fill(MET_Parallel_Mag-PT_true.Mag());
            hist_MET_Perpendicular->Fill(MET_Perpendicular_Mag);
@@ -120,6 +133,8 @@ TGraphErrors* TGE_TF1(TGraphErrors *plot, TF1 *fit_func)
            }
            hist_PT_true->Fill(pythia_JET_PT);
            vect_SumPt.push_back(pythia_JET_PT);
+           TVector3 MET_RECO = Smear_MET(PT_true, pythia_JET_PT);
+           hist_MET_RECO->Fill(MET_RECO.Mag());
            for(int i=0; i<HT_Size; i++)
            {
                delphes_JET_PT=HT;
@@ -249,6 +264,13 @@ TGraphErrors* TGE_TF1(TGraphErrors *plot, TF1 *fit_func)
         hist_PT_true->Draw();
         c5->SaveAs("Sum_PT_MC.png");
         
+        c7->cd();
+        hist_MET_RECO->GetXaxis()->SetTitle("MET RECO [GeV]");
+        hist_MET_RECO->GetYaxis()->SetTitle("Number of Events");
+        hist_MET_RECO->SetLineColor(kBlack);
+        hist_MET_RECO->Draw();
+        c7->SaveAs("MET_RECO.png");
+        
         int Parallel_entries=0;
         int Perpendicular_entries=0;
         double sigmaMETperp[Ninterval];
@@ -296,253 +318,143 @@ TGraphErrors* TGE_TF1(TGraphErrors *plot, TF1 *fit_func)
         
         }
         
-        cout << endl << endl << "Fitting Perpendicular TGE" << endl;
-        
-        TCanvas *c6 = new TCanvas("c6","Canvas for TGE Perp",750,500);
-        
-        //TCanvas *c7 = new TCanvas("c7","Canvas for TGE Par",750,500);
-        
-        
-        TPad *pad1 = new TPad("pad1","pad1",0,0.3,1,1.0);
-        pad1->SetBottomMargin(0.95);
-        //pad1->SetLogy();
-        pad1->Draw();
-        pad1->cd();
-        
         TGraphErrors *gr_perp = new TGraphErrors(Ninterval, sumPT, sigmaMETperp, sumPT_err, sigmaMETperp_err);
         
         TGraphErrors *gr_par = new TGraphErrors(Ninterval, sumPT, sigmaMETpar, sumPT_err, sigmaMETpar_err);
         
-        gr_perp->SetMarkerColor(kRed);
-        gr_perp->SetLineColor(kRed);
-        gr_par->SetMarkerColor(kBlue);
-        gr_par->SetLineColor(kBlue);
+        //push this into one function
+        Plot_Res_vs_SumPT(gr_perp,gr_par);
         
-        
-        TMultiGraph* mg = new TMultiGraph();
-        mg->Add(gr_perp);
-        mg->Add(gr_par);
-        mg->Draw("AP");
-        
-        /*c6->cd();
-        gr_perp->Draw("AP");
-        c6->SaveAs("Resolution_Perp_vs_SumPT.png");
-        
-        c7->cd();
-        gr_par->Draw("AP");
-        c7->SaveAs("Resolution_Par_vs_SumPT.png");*/
-        
-        double con1_perp=0.02;
-        double con2_perp=0.1;
-        double con1_par=0.05;
-        double con2_par=0.1;
-        
-        TF1 *func_perp = new TF1("func_perp", Fit_sqrt,0.0, 1200.0, 2);
-        func_perp->SetParameter(0,con1_perp);
-        func_perp->SetParameter(1,con2_perp);
-        func_perp->SetParName(0,"con1_perp");
-        func_perp->SetParName(1,"con2_perp");
-        func_perp->SetNpx(10000000);
-        func_perp->SetLineColor(kYellow);
-        gr_perp->Fit(func_perp,"EM");
-        
-        cout << "Fitting Parallel TGE" << endl;
-        
-        TF1 *func_par = new TF1("func_par", Fit_sqrt,0.0, 1200.0, 2);
-        func_par->SetParameter(0,con1_par);
-        func_par->SetParameter(1,con2_par);
-        func_par->SetParName(0,"con1_par");
-        func_par->SetParName(1,"con2_par");
-        func_par->SetNpx(10000000);
-        func_par->SetLineColor(kGreen);
-        gr_par->Fit(func_par,"EM");
-        
-        
-        mg->GetXaxis()->SetTitle("SumPt [GeV]");
-        mg->GetYaxis()->SetTitle("Resolution/SumPt [GeV]");
-        mg->GetXaxis()->SetTitleSize(0.047);
-        mg->GetYaxis()->SetTitleSize(0.047);
-        mg->GetXaxis()->SetLabelSize(0.05);
-        mg->GetYaxis()->SetLabelSize(0.05);
-        
-        TLegend* leg = new TLegend(0.7,0.7,.9,.9, "");
-        leg->SetTextFont(42);
-        leg->SetTextSize(0.04);
-        leg->SetFillColor(kWhite);
-        leg->SetTextColor(kBlack);
-        leg->AddEntry(gr_perp,"Perpendicular","L");
-        leg->AddEntry(gr_par,"Parallel","L");
-        leg->Draw("SAME");
-        
-        
-        c6->cd();
-        TPad *pad2 = new TPad("pad2", "pad2", 0, 0.05, 1, 0.3);
-        pad2->Draw();
-        pad2->cd();
-        pad2->SetTopMargin(0.05);
-        pad2->Update();
-        c6->Update();
-        
-        TGraphErrors* residual_perp=TGE_TF1(gr_perp,func_perp);
-        residual_perp->SetMarkerColor(kRed);
-        residual_perp->SetLineColor(kRed);
-        
-        TGraphErrors* residual_par=TGE_TF1(gr_par,func_par);
-        residual_par->SetMarkerColor(kBlue);
-        residual_par->SetLineColor(kBlue);
-        TMultiGraph* mg_res = new TMultiGraph();
-        mg_res->Add(residual_perp);
-        mg_res->Add(residual_par);
-        mg_res->Draw("AP");
-        mg_res->GetXaxis()->SetLabelSize(0.12);
-        mg_res->GetYaxis()->SetLabelSize(0.12);
-        
-        TLine* line = new TLine(0.0,0.0,1048.0,0.0);
-        line->SetLineColor(kBlack);
-        line->SetLineStyle(2);
-        line->Draw();
-        pad2->Update();
-        c6->Update();
-        
-        c6->SaveAs("Resolution_vs_SumPT.png");
     }
 
-/*
- if(vect_SumPt.at(k)<50.0)
- {
- arr_Sum_Sum_Pt[0]=vect_SumPt.at(k)+arr_Sum_Sum_Pt[0];
- arr_Total_Sum_Pt[0]=1.0+arr_Total_Sum_Pt[0];
- //vect_hist_MET_Parallel.at(0)->SetBins(1000,-200.0,200.0);
- vect_hist_MET_Parallel.at(0)->Fill(vect_MET_Parallel.at(k));
- vect_hist_MET_Perpendicular.at(0)->Fill(vect_MET_Perpendicular.at(k));
- }
- else if(vect_SumPt.at(k) > 50.0 && vect_SumPt.at(k)<100.0)
- {
- arr_Sum_Sum_Pt[1]=vect_SumPt.at(k)+arr_Sum_Sum_Pt[1];
- arr_Total_Sum_Pt[1]=1.0+arr_Total_Sum_Pt[1];
- //vect_hist_MET_Parallel.at(0)->SetBins(1000,-200.0,200.0);
- vect_hist_MET_Parallel.at(1)->Fill(vect_MET_Parallel.at(k));
- vect_hist_MET_Perpendicular.at(1)->Fill(vect_MET_Perpendicular.at(k));
- }
- else if(vect_SumPt.at(k) > 100.0 && vect_SumPt.at(k)<150.0)
- {
- arr_Sum_Sum_Pt[2]=vect_SumPt.at(k)+arr_Sum_Sum_Pt[2];
- arr_Total_Sum_Pt[2]=1.0+arr_Total_Sum_Pt[2];
- //vect_hist_MET_Parallel.at(0)->SetBins(1000,-200.0,200.0);
- vect_hist_MET_Parallel.at(2)->Fill(vect_MET_Parallel.at(k));
- vect_hist_MET_Perpendicular.at(2)->Fill(vect_MET_Perpendicular.at(k));
- }
- else if(vect_SumPt.at(k) > 150.0 && vect_SumPt.at(k)<200.0)
- {
- arr_Sum_Sum_Pt[3]=vect_SumPt.at(k)+arr_Sum_Sum_Pt[3];
- arr_Total_Sum_Pt[3]=1.0+arr_Total_Sum_Pt[3];
- //vect_hist_MET_Parallel.at(0)->SetBins(1000,-200.0,200.0);
- vect_hist_MET_Parallel.at(3)->Fill(vect_MET_Parallel.at(k));
- vect_hist_MET_Perpendicular.at(3)->Fill(vect_MET_Perpendicular.at(k));
- }
- else if(vect_SumPt.at(k) > 200.0 && vect_SumPt.at(k)<225.0)
- {
- arr_Sum_Sum_Pt[4]=vect_SumPt.at(k)+arr_Sum_Sum_Pt[4];
- arr_Total_Sum_Pt[4]=1.0+arr_Total_Sum_Pt[4];
- //vect_hist_MET_Parallel.at(0)->SetBins(1000,-200.0,200.0);
- vect_hist_MET_Parallel.at(4)->Fill(vect_MET_Parallel.at(k));
- vect_hist_MET_Perpendicular.at(4)->Fill(vect_MET_Perpendicular.at(k));
- }
- else if(vect_SumPt.at(k) > 225.0 && vect_SumPt.at(k)<250.0)
- {
- arr_Sum_Sum_Pt[5]=vect_SumPt.at(k)+arr_Sum_Sum_Pt[5];
- arr_Total_Sum_Pt[5]=1.0+arr_Total_Sum_Pt[5];
- //vect_hist_MET_Parallel.at(0)->SetBins(1000,-200.0,200.0);
- vect_hist_MET_Parallel.at(5)->Fill(vect_MET_Parallel.at(k));
- vect_hist_MET_Perpendicular.at(5)->Fill(vect_MET_Perpendicular.at(k));
- }
- else if(vect_SumPt.at(k) > 250.0 && vect_SumPt.at(k)<275.0)
- {
- arr_Sum_Sum_Pt[6]=vect_SumPt.at(k)+arr_Sum_Sum_Pt[6];
- arr_Total_Sum_Pt[6]=1.0+arr_Total_Sum_Pt[6];
- //vect_hist_MET_Parallel.at(0)->SetBins(1000,-200.0,200.0);
- vect_hist_MET_Parallel.at(6)->Fill(vect_MET_Parallel.at(k));
- vect_hist_MET_Perpendicular.at(6)->Fill(vect_MET_Perpendicular.at(k));
- }
- else if(vect_SumPt.at(k) > 275.0 && vect_SumPt.at(k)<300.0)
- {
- arr_Sum_Sum_Pt[7]=vect_SumPt.at(k)+arr_Sum_Sum_Pt[7];
- arr_Total_Sum_Pt[7]=1.0+arr_Total_Sum_Pt[7];
- //vect_hist_MET_Parallel.at(0)->SetBins(1000,-200.0,200.0);
- vect_hist_MET_Parallel.at(7)->Fill(vect_MET_Parallel.at(k));
- vect_hist_MET_Perpendicular.at(7)->Fill(vect_MET_Perpendicular.at(k));
- }
- else if(vect_SumPt.at(k) > 300.0 && vect_SumPt.at(k)<325.0)
- {
- arr_Sum_Sum_Pt[8]=vect_SumPt.at(k)+arr_Sum_Sum_Pt[8];
- arr_Total_Sum_Pt[8]=1.0+arr_Total_Sum_Pt[8];
- //vect_hist_MET_Parallel.at(0)->SetBins(1000,-200.0,200.0);
- vect_hist_MET_Parallel.at(8)->Fill(vect_MET_Parallel.at(k));
- vect_hist_MET_Perpendicular.at(8)->Fill(vect_MET_Perpendicular.at(k));
- }
- else if(vect_SumPt.at(k) > 325.0 && vect_SumPt.at(k)<350.0)
- {
- arr_Sum_Sum_Pt[9]=vect_SumPt.at(k)+arr_Sum_Sum_Pt[9];
- arr_Total_Sum_Pt[9]=1.0+arr_Total_Sum_Pt[9];
- //vect_hist_MET_Parallel.at(0)->SetBins(1000,-200.0,200.0);
- vect_hist_MET_Parallel.at(9)->Fill(vect_MET_Parallel.at(k));
- vect_hist_MET_Perpendicular.at(9)->Fill(vect_MET_Perpendicular.at(k));
- }
- else if(vect_SumPt.at(k) > 350.0 && vect_SumPt.at(k)<375.0)
- {
- arr_Sum_Sum_Pt[10]=vect_SumPt.at(k)+arr_Sum_Sum_Pt[10];
- arr_Total_Sum_Pt[10]=1.0+arr_Total_Sum_Pt[10];
- //vect_hist_MET_Parallel.at(0)->SetBins(1000,-200.0,200.0);
- vect_hist_MET_Parallel.at(10)->Fill(vect_MET_Parallel.at(k));
- vect_hist_MET_Perpendicular.at(10)->Fill(vect_MET_Perpendicular.at(k));
- }
- else if(vect_SumPt.at(k) > 375.0 && vect_SumPt.at(k)<400.0)
- {
- arr_Sum_Sum_Pt[11]=vect_SumPt.at(k)+arr_Sum_Sum_Pt[11];
- arr_Total_Sum_Pt[11]=1.0+arr_Total_Sum_Pt[11];
- //vect_hist_MET_Parallel.at(0)->SetBins(1000,-200.0,200.0);
- vect_hist_MET_Parallel.at(11)->Fill(vect_MET_Parallel.at(k));
- vect_hist_MET_Perpendicular.at(11)->Fill(vect_MET_Perpendicular.at(k));
- }
- else if(vect_SumPt.at(k) > 400.0 && vect_SumPt.at(k)<450.0)
- {
- arr_Sum_Sum_Pt[12]=vect_SumPt.at(k)+arr_Sum_Sum_Pt[12];
- arr_Total_Sum_Pt[12]=1.0+arr_Total_Sum_Pt[12];
- //vect_hist_MET_Parallel.at(0)->SetBins(1000,-200.0,200.0);
- vect_hist_MET_Parallel.at(12)->Fill(vect_MET_Parallel.at(k));
- vect_hist_MET_Perpendicular.at(12)->Fill(vect_MET_Perpendicular.at(k));
- }
- else if(vect_SumPt.at(k) > 450.0 && vect_SumPt.at(k)<500.0)
- {
- arr_Sum_Sum_Pt[13]=vect_SumPt.at(k)+arr_Sum_Sum_Pt[13];
- arr_Total_Sum_Pt[13]=1.0+arr_Total_Sum_Pt[13];
- //vect_hist_MET_Parallel.at(0)->SetBins(1000,-200.0,200.0);
- vect_hist_MET_Parallel.at(13)->Fill(vect_MET_Parallel.at(k));
- vect_hist_MET_Perpendicular.at(13)->Fill(vect_MET_Perpendicular.at(k));
- }
- else if(vect_SumPt.at(k) > 500.0 && vect_SumPt.at(k)<600.0)
- {
- arr_Sum_Sum_Pt[14]=vect_SumPt.at(k)+arr_Sum_Sum_Pt[14];
- arr_Total_Sum_Pt[14]=1.0+arr_Total_Sum_Pt[14];
- //vect_hist_MET_Parallel.at(0)->SetBins(1000,-200.0,200.0);
- vect_hist_MET_Parallel.at(14)->Fill(vect_MET_Parallel.at(k));
- vect_hist_MET_Perpendicular.at(14)->Fill(vect_MET_Perpendicular.at(k));
- }
- else if(vect_SumPt.at(k) > 600.0 && vect_SumPt.at(k)<800.0)
- {
- arr_Sum_Sum_Pt[15]=vect_SumPt.at(k)+arr_Sum_Sum_Pt[15];
- arr_Total_Sum_Pt[15]=1.0+arr_Total_Sum_Pt[15];
- //vect_hist_MET_Parallel.at(0)->SetBins(1000,-200.0,200.0);
- vect_hist_MET_Parallel.at(15)->Fill(vect_MET_Parallel.at(k));
- vect_hist_MET_Perpendicular.at(15)->Fill(vect_MET_Perpendicular.at(k));
- }
- else if(vect_SumPt.at(k) > 800.0)// && vect_SumPt.at(k)<800.0)
- {
- arr_Sum_Sum_Pt[16]=vect_SumPt.at(k)+arr_Sum_Sum_Pt[16];
- arr_Total_Sum_Pt[16]=1.0+arr_Total_Sum_Pt[16];
- //vect_hist_MET_Parallel.at(0)->SetBins(1000,-200.0,200.0);
- vect_hist_MET_Parallel.at(16)->Fill(vect_MET_Parallel.at(k));
- vect_hist_MET_Perpendicular.at(16)->Fill(vect_MET_Perpendicular.at(k));
- }
- }
- */
+
+void Plot_Res_vs_SumPT(TGraphErrors* gr_perp, TGraphErrors* gr_par)
+{
+    TCanvas *c6 = new TCanvas("c6","Canvas for TGE Perp",750,500);
+
+    TPad *pad1 = new TPad("pad1","pad1",0,0.3,1,1.0);
+    pad1->SetBottomMargin(0.95);
+    //pad1->SetLogy();
+    pad1->Draw();
+    pad1->cd();
+
+    gr_perp->SetMarkerColor(kRed);
+    gr_perp->SetLineColor(kRed);
+    gr_par->SetMarkerColor(kBlue);
+    gr_par->SetLineColor(kBlue);
+
+
+    TMultiGraph* mg = new TMultiGraph();
+
+
+    double con0_perp=0.2;
+    double con1_perp=0.02;
+    double con2_perp=0.1;
+    double con0_par=0.2;
+    double con1_par=0.05;
+    double con2_par=0.1;
+
+    cout << endl << endl << "Fitting Perpendicular TGE" << endl;
+
+    TF1 *func_perp = new TF1("func_perp", Fit_sqrt,0.0, 1200.0, 3);
+    func_perp->SetParameter(0,con0_perp);
+    func_perp->SetParameter(1,con1_perp);
+    func_perp->SetParameter(2,con2_perp);
+    func_perp->SetParName(0,"con0_perp");
+    func_perp->SetParName(1,"con1_perp");
+    func_perp->SetParName(2,"con2_perp");
+    func_perp->SetParLimits(0,0.0,10.0);
+    func_perp->SetParLimits(1,0.0,1.0);
+    func_perp->SetParLimits(2,0.0,1.0);
+    func_perp->SetNpx(10000000);
+    func_perp->SetLineColor(kYellow);
+    gr_perp->Fit(func_perp,"EM");
+    mg->Add(gr_perp);
+
+    cout << "Fitting Parallel TGE" << endl;
+
+    TF1 *func_par = new TF1("func_par", Fit_sqrt,0.0, 1200.0, 3);
+    func_par->SetParameter(0,con0_par);
+    func_par->SetParameter(1,con1_par);
+    func_par->SetParameter(2,con2_par);
+    func_par->SetParName(0,"con0_par");
+    func_par->SetParName(1,"con1_par");
+    func_par->SetParName(2,"con2_par");
+    func_par->SetParLimits(0,0.0,10.0);
+    func_par->SetParLimits(1,0.0,1.0);
+    func_par->SetParLimits(2,0.0,1.0);
+    func_par->SetNpx(10000000);
+    func_par->SetLineColor(kGreen);
+    gr_par->Fit(func_par,"EM");
+    mg->Add(gr_par);
+
+    pad1->Update();
+
+    mg->Draw("AP");
+    mg->GetXaxis()->SetTitle("SumPt [GeV]");
+    mg->GetYaxis()->SetTitle("Resolution/SumPt [GeV]");
+    mg->GetXaxis()->SetTitleSize(0.047);
+    mg->GetYaxis()->SetTitleSize(0.047);
+    mg->GetXaxis()->SetLabelSize(0.05);
+    mg->GetYaxis()->SetLabelSize(0.05);
+    pad1->Update();
+
+    TPaveStats* stats_par=(TPaveStats*)(gr_par->GetListOfFunctions()->FindObject("stats"));
+    stats_par->SetY1NDC(0.5);
+    stats_par->SetY2NDC(0.7);
+    stats_par->SetTextColor(kBlue);
+    TPaveStats* stats_perp=(TPaveStats*)(gr_perp->GetListOfFunctions()->FindObject("stats"));
+    stats_perp->SetTextColor(kRed);
+
+    pad1->Modified();
+
+    c6->cd();
+    TPad *pad2 = new TPad("pad2", "pad2", 0, 0.05, 1, 0.3);
+    pad2->Draw();
+    pad2->cd();
+    pad2->SetTopMargin(0.05);
+    pad2->Update();
+    c6->Update();
+
+    TGraphErrors* residual_perp=TGE_TF1(gr_perp,func_perp);
+    residual_perp->SetMarkerColor(kRed);
+    residual_perp->SetLineColor(kRed);
+
+    TGraphErrors* residual_par=TGE_TF1(gr_par,func_par);
+    residual_par->SetMarkerColor(kBlue);
+    residual_par->SetLineColor(kBlue);
+    TMultiGraph* mg_res = new TMultiGraph();
+    mg_res->Add(residual_perp);
+    mg_res->Add(residual_par);
+    mg_res->Draw("AP");
+    mg_res->GetXaxis()->SetLabelSize(0.12);
+    mg_res->GetYaxis()->SetLabelSize(0.12);
+
+    TLine* line = new TLine(0.0,0.0,1048.0,0.0);
+    line->SetLineColor(kBlack);
+    line->SetLineStyle(2);
+    line->Draw();
+    pad2->Update();
+    c6->Update();
+
+    c6->SaveAs("Resolution_vs_SumPT.png");
+}
+
+
+
+TVector3 Smear_MET(const TVector3& PT_true, double sum_PT)
+{
+    TVector3 zhat;
+    zhat.SetXYZ(0.0,0.0,1.0);
+  
+    double sigma_perp = sum_PT*sqrt((4.20687*4.20687)/(sum_PT*sum_PT)+(0.548129*0.548129)/sum_PT+0.0116634*0.0116634);
+    double sigma_par  = sum_PT*sqrt((4.02779*4.02779)/(sum_PT*sum_PT)+(0.512707*0.512707)/sum_PT+0.0431817*0.0431817);
+    
+    TVector3 parhat  = PT_true.Unit();
+    TVector3 perphat = PT_true.Cross(zhat).Unit();
+    
+    TVector3 MET = PT_true + gRandom->Gaus(0.,sigma_par)*parhat + gRandom->Gaus(0.,sigma_perp)*perphat;
+    
+    return MET;
+    
+}
